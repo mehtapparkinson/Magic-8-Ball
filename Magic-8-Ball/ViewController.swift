7ad2bd1a6f718ccc721c217eda307543512afe02
Mehtap <97293905+mehtapparkinson@users.noreply.github.com>
2023-12-05 13:50:59 +0100

    Initial Commit

---
+++ Magic-8-Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic-8-Ball
+//
+//  Created by Mehtap Parkinson on 5/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
