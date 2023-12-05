6ce5875d4ea6e03295a8d4b9bb03fade795e0ed4
Mehtap <97293905+mehtapparkinson@users.noreply.github.com>
2023-12-05 14:23:42 +0100

    Added Magic 8 Ball project

--- Magic-8-Ball/ViewController.swift
+++ Magic-8-Ball/ViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var AnswerText: UITextField!
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            super.viewDidLoad()
+            AnswerText.text = "Hi you can ask me anything"
+        }
+
+ 
+    @IBAction func AskButton(_ sender: Any) {
+        let magic8BallResponses = [
+            // Positive Answers
+            "It is certain.",
+            "It is decidedly so.",
+            "Without a doubt.",
+            "Yes – definitely.",
+            "You may rely on it.",
+            "As I see it, yes.",
+            "Most likely.",
+            "Outlook good.",
+            "Yes.",
+            "Signs point to yes.",
+            
+            // Non-Committal Answers
+            "Reply hazy, try again.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            
+            // Negative Answers
+            "Don't count on it.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Very doubtful."
+        ]
+
+        let randomResponse = magic8BallResponses.randomElement()
+        AnswerText.text=randomResponse
     }
-
-
+    
 }
 
